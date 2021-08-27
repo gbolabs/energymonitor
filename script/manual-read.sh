9b2d35f5eef1e0c90353753f4bfdc3009ce3c2f5
Gautier Boder <gboder@outlook.com>
2021-08-27 13:45:17 +0200

    code

--- script/manual-read.sh
+++ script/manual-read.sh
@@ -1,1 +1,3 @@
 # This script triggers a read and display the result on screen
+echo -n -e '\x2F\x3F\x21\x0D\x0A' > /dev/ttyUSB0
+dd if=/dev/ttyUSB0 bs=1 of=response.txt
---
+++ src/e230-reader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace e230_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
